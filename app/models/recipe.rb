a77b60153cf7a27511396cb3dc2398ddb1495004
Rhys Malyon <malyonr@gmail.com>
2021-09-17 21:10:28 +0900

    Add edit link to recipe on profile

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,6 @@ class Recipe < ApplicationRecord
   has_many :bookmarks
   has_many :reviews, dependent: :destroy
 
-  validates :name, :ingredients, :prep_time, :instructions, :rating, presence: true
+  validates :name, :ingredients, :prep_time, :instructions, presence: true
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end

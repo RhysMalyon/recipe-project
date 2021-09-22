a4b503f5d2e358048c6b9e465d5b4f4bba1f4524
Rhys Malyon <malyonr@gmail.com>
2021-09-22 10:57:21 +0900

    Prevent sending blank values

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,13 @@
 class Recipe < ApplicationRecord
+  before_save :remove_blank_ingredients
   belongs_to :user
   has_many :bookmarks
   has_many :reviews, dependent: :destroy
 
   validates :name, :ingredients, :prep_time, :instructions, presence: true
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+
+  def remove_blank_ingredients
+    ingredients.reject!(&:blank?)
+  end
 end

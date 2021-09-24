8b2e41db9a28a48e75f02ae7f53d0cc918962216
David Gregorato <HydroSnake@gmail.com>
2021-09-24 13:56:34 +0900

    Refactored the recipe tests.

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -3,30 +3,33 @@ require 'rails_helper'
 require_relative '../app/models/recipe'
 
 RSpec.describe Recipe, type: :model do
-  before { @recipe = Recipe.new(name: 'Green eggs and Ham', ingredients: ['eggs', 'ham'], prep_time: 15, instructions: 'Crack the eggs, and put in the ham.', rating: 4) } # We declare an instance @recipe that we can reuse in this series of tests, instead of having to create instances for every test.
+  let(:recipe) { Recipe.new(name: 'Green eggs and Ham', ingredients: ['eggs', 'ham'], prep_time: 15, instructions: 'Crack the eggs, and put in the ham.', rating: 4) }
 
-  it 'has an name' do
-    name = @recipe.name
-
-    expect(name).to eq('Green eggs and Ham')
+  it 'has a name' do
+    expect(recipe.name).to eq('Green eggs and Ham')
   end
 
-  it 'has a list of ingredients' do
-    ingredients = @recipe.ingredients
+  context 'ingredients' do
+    it 'has a list of ingredients' do
+      expect(recipe.ingredients).to eq(%w[eggs ham])
+    end
 
-    expect(ingredients).to eq(%w[eggs ham])
+    it 'allows ingredients to be added' do
+      expect(recipe.ingredients.push('green stuff')).to eq(['eggs', 'ham', 'green stuff'])
+    end
+
+    it 'allows ingredients to be removed' do
+      recipe.ingredients.pop
+      expect(recipe.ingredients).to eq(['eggs'])
+    end
   end
 
   it 'has a prep time' do
-    prep_time = @recipe.prep_time
-
-    expect(prep_time).to eq(15)
+    expect(recipe.prep_time).to eq(15)
   end
 
   it 'has instructions' do
-    instructions = @recipe.instructions
-
-    expect(instructions).to eq('Crack the eggs, and put in the ham.')
+    expect(recipe.instructions).to eq('Crack the eggs, and put in the ham.')
   end
 
   it 'has an author'
--- spec/review_spec.rb
+++ spec/review_spec.rb
@@ -3,16 +3,14 @@ require 'rails_helper'
 require_relative '../app/models/review'
 
 RSpec.describe Review, type: :model do
-  before { @review = Review.new(content: 'It was a good recipe', rating: 4) }
+  let(:review) { Review.new(content: 'It was a good recipe', rating: 4) }
 
   it 'has something written' do
-    content = @review.content
-    expect(content).to eq('It was a good recipe')
+    expect(review.content).to eq('It was a good recipe')
   end
 
   it 'has a rating' do
-    rating = @review.rating
-    expect(rating).to eq(4)
+    expect(review.rating).to eq(4)
   end
 
   it 'is attached to a recipe'

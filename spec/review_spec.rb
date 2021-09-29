1239ce51ebcd9cc20239491c78cc2cf915ee212a
David Gregorato <HydroSnake@gmail.com>
2021-09-29 17:44:43 +0900

    Finished implementing the review specs.

--- spec/review_spec.rb
+++ spec/review_spec.rb
@@ -4,6 +4,8 @@ require_relative '../app/models/review'
 
 RSpec.describe Review, type: :model do
   let(:review) { Review.new(content: 'It was a good recipe', rating: 4) }
+  let(:user) { User.new(username: 'Larry', email: 'larry@jokeaddress.com', password: '123456') }
+  let(:recipe) { Recipe.new(name: 'Green eggs and Ham', ingredients: ['eggs', 'ham'], prep_time: 15, instructions: 'Crack the eggs, and put in the ham.', rating: 4) }
 
   it 'has something written' do
     expect(review.content).to eq('It was a good recipe')
@@ -13,6 +15,13 @@ RSpec.describe Review, type: :model do
     expect(review.rating).to eq(4)
   end
 
-  it 'is attached to a recipe'
-  it 'is written by a user'
+  it 'is attached to a recipe' do
+    review.recipe = recipe
+    expect(review.recipe).to be(recipe)
+  end
+
+  it 'is written by a user' do
+    review.user = user
+    expect(review.user).to be(user)
+  end
 end

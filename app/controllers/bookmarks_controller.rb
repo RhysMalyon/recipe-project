b9cdf1f6cfe3f85deacde8df6d0d8007e4770ae0
Rhys Malyon <malyonr@gmail.com>
2021-10-08 14:38:26 +0900

    Create new action

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,3 +1,16 @@
 class BookmarksController < ApplicationController
   # Don't forget to authorize @bookmark as per pundit
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @bookmark = Bookmark.new
+    authorize @recipe
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
 end
